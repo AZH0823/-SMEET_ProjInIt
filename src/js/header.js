f1a7a664394550261394de7a5b6b57e813ca79f8
chi0410 <a0912938525@gmail.com>
2023-01-12 15:46:14 +0800

    圖片新增及修改

--- src/js/header.js
+++ src/js/header.js
@@ -51,3 +51,44 @@ $(function () {
         console.log("sss");
     })
 })
+
+$(function () {
+    $(".slide_user").on("click", function () {
+        console.log("yyy");
+
+    })
+})
+
+// $(function () {
+//     $(".abouttab_a").on("click", function () {
+//         console.log("www");
+//         $(".abouttab").attr("border-bottom: 1px solid #000");
+//         console.log("aaa")
+//     })
+// })
+
+function html_border() {
+    let activeNav = document.querySelector('.navtab.active');
+    // var activeTab = document.querySelector('.tab.active');
+    // console.log(this);
+    let currentNav = this;
+    // let currentTab = document.querySelector(this.getAttribute('href'));
+
+    // activeNav.classList.remove('active');
+    // activeTab.classList.remove('active');
+    // currentNav.classList.add('active');
+    // currentTab.classList.add('active');
+
+    // let border = document.querySelector('.navtab');
+    // border.style.border = "1px solid black";
+    // console.log(border);
+    // box.style.outline = "3px solid blue";
+}
+html_border();
+
+
+
+// var navTabs = document.querySelectorAll('.nav-tab');
+// for (var i = 0; i < navTabs.length; i++) {
+//     navTabs[i].addEventListener('click', activeTabHandler)
+// }
