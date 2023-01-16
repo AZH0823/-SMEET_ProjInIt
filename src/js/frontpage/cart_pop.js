dade3204408a86443c337f81bc61432178d8223d
yuting33143 <cora33143@gmail.com>
2023-01-16 20:18:47 +0800

    增加收藏的php

---
+++ src/js/frontpage/cart_pop.js
@@ -0,0 +1,17 @@
+// const cart_pop = Vue.createApp({
+//     data(){
+//         return{
+//             shop_data:[{}],
+//         }
+//     },
+//     mounted(){
+//         this.get_data();
+//     },
+//     methods:{
+//         // 抓localstorage資料 渲染
+//         get_data(){
+//             let a =localStorage.getItem('shoppingData');
+//             console.log('a');
+//         }
+//     }
+// }).mount('#cart_pop');
