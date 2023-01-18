5b0348b0181af1ebe32d45f0320654c1a11ccbb3
chi0410 <a0912938525@gmail.com>
2023-01-18 09:47:24 +0800

    會員連結更新，功能更新，漢堡製作中

--- src/js/header.js
+++ src/js/header.js
@@ -1,26 +1,29 @@
-$(function () {
+// $(function () {
 
-    $(".slide_ham").on("click", function () {
-        // console.log("qwert");
-        $(".header_bar").toggleClass("-on")
-        $(".user_ham").toggleClass("-on")
-    })
-})
-$(function () {
+//     $(".indexslide_ham").on("click", function () {
+//         $(".indexheader_bar").toggleClass("--on")
 
-    $(".indexslide_ham").on("click", function () {
-        // console.log("ttt");
-        $(".indexheader_bar").toggleClass("--on")
-
-        $(".indexuser_ham").toggleClass("--on")
-    })
-})
+//         $(".indexuser_ham").toggleClass("--on")
+//     })
+// })
 
 $(function () {
     $(".slide_cart").on("click", function () {
         // console.log("ttt");
         $(".cart_container").addClass("-carton")
         // console.log("sss");
+        setTimeout(function () {
+            $('.cart_bg').removeClass('none');
+        }, 500);
+        $("body").css("overflow", "hidden");
+    })
+    // 點背景關掉popup
+    $('.cart_bg').on('click', function () {
+        $('.cart_bg').addClass('none');
+        $("body").css("overflow", "auto");
+        $(".cart_container").toggleClass("-closeon")
+        $(".cart_container").removeClass("-carton")
+        $(".cart_container").removeClass("-closeon")
     })
 })
 
@@ -31,46 +34,127 @@ $(function () {
         $(".cart_container").removeClass("-carton")
         $(".cart_container").removeClass("-closeon")
         // console.log("sss");
+        $('.cart_bg').addClass('none');
+        $("body").css("overflow", "auto");
+
     })
 })
 
 $(function () {
     $(".indexslide_cart").on("click", function () {
-        // console.log("ttt");
         $(".cart_container").addClass("-carton")
-        // console.log("sss");
     })
 })
 
 $(function () {
     $(".icon_x").on("click", function () {
-        // console.log("uuu");
         $(".cart_container").toggleClass("-closeon")
         $(".cart_container").removeClass("-carton")
         $(".cart_container").removeClass("-closeon")
-        // console.log("sss");
     })
 })
 
-$(document).click(function (event) {
-    //目標
-    let ham_div = $('.ham_div');
-    // console.log("aaa")
-    // let testInput2 = $('#testInput2');
-    // if (!ham_div.is(event.target) && !testInput2.is(event.target))
-    $(".key").slideUp();
+$(document).mouseup(function (e) {
+    let _con = $('.ham_div'); // 就是你不希望被點到的 div
+    if (!_con.is(e.target) && _con.has(e.target).length === 0) {
+        // $('#header_bar').removeClass('.-on');
+        // console.log("www");
+    }
 });
 
-// document.addEventListener('click', function () {
-//     $('.ham_div').hide();
-// }, true);
 
-// document.getElementsByClassName('slide_ham').addEventListener('click', function () {
-//     $('.header_bar').slideDown(300);
-// });
-
-$(document).ready(function () {
-    $(".user_click").click(function () {
-        $(".user_pop").fadeIn("slow");
-    });
+$(".ham_div").click(function () {
+    $(body).toggleClass(".slideoverflow");
 });
+
+let my_block = document.getElementById("logo_block");
+
+window.addEventListener("scroll", function () {
+
+    //console.log( my_block.getBoundingClientRect().top );
+    // if (my_block.getBoundingClientRect().top <= 0) {
+    //     // my_block.addClass("slide_header");
+    //     // console.log('hi');
+    // }
+});
+
+// slide頁漢堡點擊收合
+let ham_body = document.querySelector('body');
+// console.log(ham_body);
+$(".slide_ham").on("click", function () {
+    // console.log("qwert");
+    $(".header_bar").toggleClass("-on");
+    $(".user_ham").toggleClass("-on");
+    $("#ham_bg").toggleClass("hide");
+    $("#ham_bg").toggleClass("n_none");
+    ham_body.style.overflow = "hidden";
+
+
+})
+//slide頁燈箱點擊收合
+
+$("#ham_bg").on("click", function () {
+    $(".header_bar").toggleClass("-on");
+    $(".user_ham").toggleClass("-on");
+    $("#ham_bg").toggleClass("hide");
+    $("#ham_bg").toggleClass("n_none");
+    ham_body.style.overflow = "auto";
+
+})
+function ham_pop() {
+    let ham_bg = document.getElementById('ham_bg');
+    // let login = document.getElementById('login');
+    // let body = document.querySelector('body');
+    //打開popup
+    // console.log("sss");
+
+
+    ham_bg.classList.toggle("hide");
+    ham_bg.classList.toggle("n_none");
+    $(".header_bar").toggleClass("-on")
+    $(".user_ham").toggleClass("-on")
+}
+
+
+
+// index頁漢堡點擊收合
+let indexham_body = document.querySelector('body');
+// console.log(ham_body);
+$(".indexslide_ham").on("click", function () {
+    // console.log("qwert");
+    indexham_body.style.overflow = "auto";
+
+    $(".indexheader_bar").toggleClass("--on");
+    $(".indexuser_ham").toggleClass("--on");
+    $("#indexham_bg").toggleClass("indexhide");
+    $("#indexham_bg").toggleClass("indexn_none");
+    // let indexhamSlide = $(".indexslide_ham");
+    // console.log(e.target)
+    // if (e.target === indexhamSlide[0] || e.target == "ii_ham") {
+    //     console.log(e.target.style.overflow);
+    //     if (e.target.style.overflow == "auto") {
+    //         indexham_body.style.overflow = "hidden";
+    //     } else {
+    //         $(indexhamSlide).removeAttr("overflow", "hidden");
+    //         // indexham_body.style.overflow = "";
+    //     }
+    // }
+    // if(e.target){
+
+    // }
+
+    // if ("") "xxx" else{"xxx"}; 
+
+})
+$("indexslide_ham").on("click", function () {
+
+})
+//index頁燈箱點擊收合
+
+$("#indexham_bg").on("click", function () {
+    $(".indexheader_bar").toggleClass("--on");
+    $(".indexuser_ham").toggleClass("--on");
+    $("#indexham_bg").toggleClass("indexhide");
+    $("#indexham_bg").toggleClass("indexn_none");
+    indexham_body.style.overflow = "auto";
+})
