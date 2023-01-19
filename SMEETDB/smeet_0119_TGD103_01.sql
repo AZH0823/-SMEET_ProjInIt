fe72a63804f15207f8cf44f3991f6f718bf23f22
AZH0823 <qoo9986360@gmail.com>
2023-01-19 20:44:43 +0800

    重新上傳DB資料

--- SMEETDB/smeet_0119_TGD103_01.sql
+++ SMEETDB/smeet_0119_TGD103_01.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `smeet` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `smeet`;
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: smeet
@@ -491,4 +493,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-01-19  2:30:26
+-- Dump completed on 2023-01-19 20:37:02
