6c7c9f25b43bc03e392af2bdb407071a4aff3f7d
Danny <100195638+DannyLiii@users.noreply.github.com>
2023-01-24 20:23:42 +0800

    主頁、廚師管理頁面更新

---
+++ src/php/frontpage/index_shop.php
@@ -0,0 +1,42 @@
+<?php
+
+    include("../conectDB/Connection.php");// FIXME 连结修正
+    //---------------------------------------------------
+
+
+    //建立SQL語法
+    $sql = "SELECT d.ID, `Type`, d.Name,`Price`, `IMG`, `shopPoint`, `Introduction`, `Condition`, `ProductType`
+    FROM Dish d
+    join DishsType dt
+    on d.`type` = dt.ID 
+    where ProductType = '商城冷凍'
+    limit 8";
+   
+    //執行
+    $statement = getPDO()->prepare($sql); 
+    $statement->execute();
+    $data = $statement->fetchAll();
+
+    $arr = [];
+
+    foreach($data as $newData){
+        $arr[] = array(
+            'ID'=>$newData['ID'],
+            'Type' => $newData['Type'],
+            'Name'=> $newData['Name'],
+            'Price'=> $newData['Price'],
+            'IMG'=> $newData['IMG'],
+            'shopPoint'=> $newData['shopPoint'],
+            'Introduction'=> $newData['Introduction'],
+            'Condition'=> $newData['Condition'],
+            'ProductType'=> $newData['ProductType'],
+        );
+    }
+
+
+    //回傳json
+    echo json_encode($arr); 
+    // echo json_encode($data); 
+
+    
+?>
