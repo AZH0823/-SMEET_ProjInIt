ed482cf283056575526beaa2ce5656b2b548c53a
ericcai0814 <d4070707@gmail.com>
2023-01-25 21:04:10 +0800

    1、導覽列樣式修正 2、修正商品管理updateAPI

--- src/php/backpage/back_productManger_u.php
+++ src/php/backpage/back_productManger_u.php
@@ -3,25 +3,49 @@
     include("../conectDB/Connection.php");
     //---------------------------------------------------
     //取得POST過來的值
-    $dish_Name = $_POST['dish_Name'] ;
-    $dish_Condition = $_POST['dish_Condition'] ;
+    $dish_ID = $_POST["dish_ID"]; //PK
+    $dish_Condition = $_POST['dish_Condition'] ; //狀態 0:下架, 1:上架,
 
-    //取得POST過來的值
-    $CID = $_POST["CID"]; //PK
-    $CateName = $_POST["CateName"]; //分類名稱
-    $Status = $_POST["Status"];   //狀態 0:刪除, 1:下架, 2:上架
-
-    //建立SQL
-    $update = "UPDATE Dish set `Condition` = 0 where ID = ?";
+    // 確認ID
+    $selectDish = "SELECT * FROM Dish WHERE ID = :DishID";
     
     //執行
-    $statement = getPDO()->prepare($sql);     
-    $statement->bindValue(1 , $CateName); 
-    $statement->bindValue(2 , $Status);
-    $statement->bindValue(3 , $CID);
+    $statement = getPDO()->prepare($selectDish);     
+    $statement->bindValue(':DishID', $dish_ID); 
     $statement->execute();
-    
-    //導頁    
-    echo "修改分類成功!";
+    $result = $statement -> fetchAll();
+
+    if(count($result) !== 0 ){
+        // 單筆資料更新
+        echo "更新資料".$dish_ID."</br>";
+
+        // 更新資料
+        $updateDish = "UPDATE Dish SET `Condition`
+        = :Condition WHERE ID = :DishID";
+
+        $statement = getPDO()->prepare($updateDish);     
+        $statement->bindValue(':Condition', $dish_Condition); 
+        $statement->bindValue(':DishID', $dish_ID); 
+        $statement->execute();
+        $result = $statement -> fetchAll();
+
+        // 檢查更新是否一樣
+        $checkedDish = "SELECT * FROM Dish WHERE ID = :DishID";
+
+        $statement = getPDO()->prepare($checkedDish);     
+        $statement->bindValue(':DishID', $dish_ID); 
+        $statement->execute();
+        $checkedResult = $statement -> fetch(); // TODO 為啥是fetch?
+        
+        // 回傳檢查結果
+        if($checkedResult["Condition"] == $dish_Condition){
+            echo '成功'."<br>";
+
+        }else{
+            echo '失敗'."<br>";
+        }
+    }else{
+        echo "id not exist"."<br>";
+    }
 
 ?>
