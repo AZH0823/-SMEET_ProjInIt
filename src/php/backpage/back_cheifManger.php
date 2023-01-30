89d1931a232f683f54dfbfc8460147976dc9f9ed
Danny <100195638+DannyLiii@users.noreply.github.com>
2023-01-30 15:38:54 +0800

    私廚團隊完成、首頁修正、新增後台私廚團隊PHP、製作後臺私廚團隊

---
+++ src/php/backpage/back_cheifManger.php
@@ -0,0 +1,18 @@
+<?php
+
+    include("../conectDB/Connection.php");
+    
+    //建立SQL
+    $sql = "SELECT `ID`, `LederName`,`LederProfile`,`Condition`
+    FROM TEAMS";
+
+    $statement = getPDO()->prepare($sql);  
+
+    $result = $statement->execute();  
+
+    //抓出全部且依照順序封裝成一個二維陣列
+    $data = $statement->fetchAll();
+    
+    echo json_encode($data);
+ 
+?>
