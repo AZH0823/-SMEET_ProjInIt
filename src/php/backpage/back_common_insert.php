301e5ff134f92b89c98a968f5b7b7738811a226d
asik0729 <perryli1994@gmail.com>
2023-01-31 15:33:34 +0800

    QA_insert 完成

---
+++ src/php/backpage/back_common_insert.php
@@ -0,0 +1,30 @@
+<?php
+    include("../conectDB/Connection.php");
+
+    $Category = $_POST['Category'];
+    $QuestionContent = $_POST['QuestionContent'];
+    $AnswerContent = $_POST['AnswerContent'];
+
+    
+    //建立SQL
+    // $sql = "SELECT * FROM CONTACT";
+
+    // $sql = "INSERT INTO Contact (ReleaseDate,Name, Phone, Email, Category, SuggestContent, Condition)value (now(),'{$Name}','{$Phone}','{$Mail}','{$Category}','{$SuggestContent}',1)";
+
+    $sql = "INSERT INTO `smeet`.`QA` (  `ReleaseDate`,`Category`, `QuestionContent`, `AnswerContent`, `Condition`, `ModifyTime`)
+    VALUES
+    (DATE(NOW()), '{$Category}', '{$QuestionContent}', '{$AnswerContent}', 0 , NOW())";
+
+
+    $statement = getPDO()->prepare($sql);  
+    // $statement->bindValue(1, $account);  
+    // $statement->bindValue(2, $pwd);
+   $statement->execute();  
+   
+
+    //抓出全部且依照順序封裝成一個二維陣列
+    // $data = $statement->fetchAll();
+    // print_r($data);
+
+
+?>
