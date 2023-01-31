3e7427127104eba575c24a8594fd39f50e01ec84
ericcai0814 <d4070707@gmail.com>
2023-02-01 00:13:42 +0800

    修正 index_home 購物車空值 qty未清空bug

--- src/js/frontpage/cart_pop.js
+++ src/js/frontpage/cart_pop.js
@@ -102,6 +102,7 @@ let cart_pop = Vue.createApp({
                 // 如果 this.ShoppingCartList 有商品，就渲染購物車 icon
                 if(LSGetItem == [] || LSGetItem == ""){
                     car_num.innerHTML = "";
+                    car_num2.innerHTML = "";
                     return ;
                 }else{
                     // 判斷當前頁面，並同步Vue.$data.shoppingCarList
